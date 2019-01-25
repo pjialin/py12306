696f888ad19e1fcdc44f1d2a9c93bc6182032cd1
Jalin <admin@pjialin.com>
2019-01-25 20:52:30 +0800

    优化避免无座检测逻辑

--- py12306/order/order.py
+++ py12306/order/order.py
@@ -246,9 +246,12 @@ class Order:
             ticket = result.get('data.ticket').split(',')  # 余票列表
             # 这里可以判断 是真实是 硬座还是无座，避免自动分配到无座
             ticket_number = ticket[0]  # 余票
-            if ticket_number != '充足' or int(ticket_number) <= 0:
+            if ticket_number != '充足' and int(ticket_number) <= 0:
                 if self.query_ins.current_seat == SeatType.NO_SEAT:  # 允许无座
                     ticket_number = ticket[1]
+                if not int(ticket_number): # 跳过无座
+                    OrderLog.add_quick_log(OrderLog.MESSAGE_GET_QUEUE_INFO_NO_SEAT).flush()
+                    return False
 
             if result.get('data.op_2') == 'true':
                 OrderLog.add_quick_log(OrderLog.MESSAGE_GET_QUEUE_LESS_TICKET).flush()
