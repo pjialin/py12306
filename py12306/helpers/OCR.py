e97ee2deb92318b0bcd0909932f2c62aa6885063
bnpzsx <1150892585@qq.com>
2019-10-12 16:02:36 +0800

    分离本地打码

--- py12306/helpers/OCR.py
+++ py12306/helpers/OCR.py
@@ -5,7 +5,7 @@ from py12306.config import Config
 from py12306.helpers.api import API_FREE_CODE_QCR_API
 from py12306.helpers.request import Request
 from py12306.log.common_log import CommonLog
-#from py12306.vender.ruokuai.main import RKClient
+from py12306.vender.ruokuai.main import RKClient
 
 
 class OCR:
@@ -27,7 +27,18 @@ class OCR:
         self = cls()
         if Config().AUTO_CODE_PLATFORM == 'free':
             return self.get_image_by_free_site(img)
-        return self.get_image_by_ml(img)
+        return self.get_img_position_by_ruokuai(img)
+
+    def get_img_position_by_ruokuai(self, img):
+        ruokuai_account = Config().AUTO_CODE_ACCOUNT
+        soft_id = '119671'
+        soft_key = '6839cbaca1f942f58d2760baba5ed987'
+        rc = RKClient(ruokuai_account.get('user'), ruokuai_account.get('pwd'), soft_id, soft_key)
+        result = rc.rk_create(img, 6113)
+        if "Result" in result:
+            return self.get_image_position_by_offset(list(result['Result']))
+        CommonLog.print_auto_code_fail(result.get("Error", CommonLog.MESSAGE_RESPONSE_EMPTY_ERROR))
+        return None
 
     def get_image_position_by_offset(self, offsets):
         positions = []
@@ -56,15 +67,6 @@ class OCR:
         CommonLog.print_auto_code_fail(CommonLog.MESSAGE_GET_RESPONSE_FROM_FREE_AUTO_CODE)
         return None
 
-    def get_image_by_ml(self, img):
-        from py12306.helpers.ocr.ml_predict import get_coordinate
-        import base64
-
-        result = get_coordinate(base64.b64decode(img))
-        result = self.get_image_position_by_offset(result)
-        # CommonLog.print_auth_code_info("验证码识别的结果为：" + result)
-        if result:
-            return result
 
 if __name__ == '__main__':
     pass
